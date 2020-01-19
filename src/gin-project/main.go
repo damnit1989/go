e171f9996de38d5f00e5cbc2ca4cd6d2585788ae
Your Name <damnit1989@163.com>
2020-01-19 11:53:45 +0800

    edit

---
+++ src/gin-project/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	// "fmt"
+	"gin-project/modules/user/handlers"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	// "time"
+)
+
+func main() {
+	// db := conn.GetDB()
+	// defer db.Close()
+
+	//设置默认表名前缀
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return "gin_" + defaultTableName
+	}
+
+	// fmt.Println(db.GetErrors())
+	var u handlers.UserController
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(401, gin.H{"message": "pong"})
+	})
+	router.GET("/")
+	router.GET("/index", u.Index)
+	router.GET("/update", u.Update)
+	router.GET("/create", u.Create)
+	router.GET("/delete", u.Delete)
+	router.Run("0.0.0.0:8312")
+}
---
+++ src/gin-project/modules/user/handlers/UserController.go
@@ -0,0 +1,72 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gin-project/conn"
+	"gin-project/modules/user/models"
+)
+
+type UserController struct{}
+
+// 查看
+func(this *UserController) Index(c *gin.Context) {
+	db := conn.GetDB()
+	id := c.Query("id") // c.Request.URL.Query().Get("lastname") 的一种快捷方式'
+	fmt.Println(id)
+	// var user models.User
+	var user []models.User
+	// var results 
+	if id != "" {
+		db.First(&user, id)
+	} else {
+		db.Find(&user)
+	}
+	c.JSON(200, user)
+	c.JSON(200, map[string]string{
+		"message": "dddddd",
+	})
+}
+
+// 新增
+func(this *UserController) Create(c *gin.Context) {
+    db := conn.GetDB()
+    // panic(db)
+    user := models.User{
+		Name:  "lmm",
+		Email: "448559829@qq.com",
+		Age:   20,
+	}
+    db.Create(&user)
+    
+	c.JSON(200, map[string]string{
+		"message": "添加成功",
+	})
+}
+
+// 更新
+func(this *UserController) Update(c *gin.Context) {
+	db := conn.GetDB()
+	user := &models.User{}
+	update_data := map[string]interface{}{
+		"Name": "bbb",
+		"Age" : 30,
+	}
+
+	db.Model(user).Where("id = ?", "2").UpdateColumns(update_data)
+	c.JSON(200, map[string]string{
+		"message": "更新成功",
+	})
+}
+
+// 删除
+func(this *UserController) Delete(c *gin.Context) {
+	db := conn.GetDB()
+	id := c.Query("id")
+
+	user := &models.User{}
+	db.Where("id  = ?", id).Delete(user)
+	c.JSON(200, map[string]string{
+		"message": "删除成功",
+	})
+}
